d95be7dd1a3c0bd8571034d4b251870b353e86ac
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-01 18:08:39 +0200

    Add: create document from auth

--- src/routes/sign-up/sign-up.component.jsx
+++ src/routes/sign-up/sign-up.component.jsx
@@ -1,9 +1,18 @@
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
 const SignUp = ()=>{
+    const logGoogleUser = async ()=> {
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
+    } 
     return (
         <>
             <h1>
                 this is the signup page
             </h1>
+            <button onClick={logGoogleUser}>
+                Sign in with google pop up
+            </button>
         </>
     )
 }
