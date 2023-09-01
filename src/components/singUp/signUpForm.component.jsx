a6c7826fd01bc50eb35a394a0dbd8b72d787981c
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-01 19:05:58 +0200

    Add: sign up form v-model

--- src/components/singUp/signUpForm.component.jsx
+++ src/components/singUp/signUpForm.component.jsx
@@ -1,21 +1,39 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+
 const SignUpForm = ()=> {
+
+    const [formField, setFormField] = useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword } = formField;
+
+    const handleChange = (event)=> {
+        const {name, value} = event.target;
+        setFormField({...formField, [name]: value});
+    }
+
     return (
         <>
             <h1>Sign Up with your email and password</h1>
             <form onSubmit={()=>{}}>
 
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" required/>
+                <input type="text" id="name" name="displayName" onChange={handleChange} value={displayName} required/>
 
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" required/>
+                <input type="email" id="email" name="email" onChange={handleChange} value={email} required/>
 
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" required/>
+                <input type="password" id="password" name="password" onChange={handleChange} value={password} required/>
 
                 <label htmlFor="confirm">Confirm Password</label>
-                <input type="password" id="confirm" required/>
-                
+                <input type="password" id="confirm" name="confirmPassword" onChange={handleChange} value={confirmPassword} required/>
+
                 <button type="submit">SIGN UP</button>
             </form>
         </>
