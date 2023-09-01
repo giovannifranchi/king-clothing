eb0b8e7a43b032b56475be808c1c8ba73a65fdfe
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-09-01 11:58:38 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='container'>hello react template with scss and bootstrap</h1>
+    </div>
+  );
+}
+
+export default App;
