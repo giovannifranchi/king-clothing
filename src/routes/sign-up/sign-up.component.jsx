5f38cbc6b3bd4a28024d8771e7bb0c67114ec60d
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-01 18:26:26 +0200

    Add: firebase redirect method

--- src/routes/sign-up/sign-up.component.jsx
+++ src/routes/sign-up/sign-up.component.jsx
@@ -1,10 +1,26 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect, auth } from '../../utils/firebase/firebase.utils';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 
 const SignUp = ()=>{
+    
+    useEffect(async ()=> {
+        const { user } = getRedirectResult(auth);
+        if(response){
+            const userDocRef = await createUserDocumentFromAuth(user);
+        }
+    }, [])
+
     const logGoogleUser = async ()=> {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     } 
+
+    const logGoogleRedirectUser = async ()=>{
+        const { user } = await signInWithGoogleRedirect();
+    }
+
     return (
         <>
             <h1>
@@ -12,6 +28,9 @@ const SignUp = ()=>{
             </h1>
             <button onClick={logGoogleUser}>
                 Sign in with google pop up
+            </button>
+            <button onClick={logGoogleRedirectUser}>
+                Sign in with google redirect
             </button>
         </>
     )
