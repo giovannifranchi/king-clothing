04766e95d1239af04e95fcdf6e58969f29cccf5a
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-02 17:01:40 +0200

    Add: dropdown with cart completed

---
+++ src/components/cartDropdown/cartDropdown.component.jsx
@@ -0,0 +1,31 @@
+import './cartDropdown.style.scss';
+
+import Button from '../button/button.component';
+import { UserContext } from '../../contexts/users.context';
+import { CartContext } from '../../contexts/cart.context';
+import { useContext } from 'react';
+
+
+const CartDropdown = () => {
+
+    const {isOpen, cartItems} = useContext(CartContext);
+
+    return (
+        <div className={`${isOpen ? 'cart-dropdown-container' : 'd-none'}`}>
+
+            {
+                cartItems.length ? 
+                (<div className='cart-items'></div>)
+                :
+                (<div className='empty-message'>No items in cart</div>)
+            }
+
+            {
+                cartItems.length ? (<Button text='Go to Checkout' buttonType='inverted' />) : (<></>)
+            }
+        </div>
+
+    )
+}
+
+export default CartDropdown;
--- src/components/cartIcon/cartIcon.component.jsx
+++ src/components/cartIcon/cartIcon.component.jsx
@@ -1,17 +1,27 @@
 
 import './cartIcon.style.scss';
-
+import CartDropdown from '../cartDropdown/cartDropdown.component';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import { UserContext } from '../../contexts/users.context';
+import { CartContext } from '../../contexts/cart.context';
+import { useContext } from 'react';
 
 
 const CartIcon = () => {
 
+    const {isOpen, setIsOpen} = useContext(CartContext);
+
+    const toggleDropDown = ()=>{
+        setIsOpen(!isOpen);
+    }
+
     return (
-        <div className='cart-icon-container'>
+        <div onClick={toggleDropDown} className='cart-icon-container'>
 
             <ShoppingIcon className='shopping-icon' />
 
             <span className='item-count'>0</span>
+            <CartDropdown/>
         </div>
     )
 }
