acbe5cede889cd874cdb74b4de09370eaa16b1e9
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-02 18:21:10 +0200

    Add: price working

--- src/components/dropdownCard/dropDownCard.component.jsx
+++ src/components/dropdownCard/dropDownCard.component.jsx
@@ -5,12 +5,13 @@ const DropdownCard = ({item})=> {
 
     const {info, itemAmount } = item;
 
-    const { name, imageUrl } = info;
+    const { name, imageUrl, price } = info;
 
     return (
         <div>
             <span>{name}</span>
             <span>{itemAmount}</span>
+            <span>{price * itemAmount}</span>
         </div>
     )
 }
