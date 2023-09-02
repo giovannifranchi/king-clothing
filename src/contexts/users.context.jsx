63b97b606e14c2b181e3b3429e9ea9acf10eaa43
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-02 15:00:01 +0200

    Add: user Context working

--- src/contexts/users.context.jsx
+++ src/contexts/users.context.jsx
@@ -1,4 +1,7 @@
 import { createContext, useState } from "react";
+import { subscribeToAuthChange } from "../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
     currentUser: null,
@@ -9,6 +12,14 @@ export const UserProvider = ({children})=>{
   
     const [currentUser, setCurrentUser] = useState(null);
     const value = {currentUser, setCurrentUser}
+
+    useEffect(()=>{
+        const unsubscribe = subscribeToAuthChange((user)=>{
+            if(user) createUserDocumentFromAuth(user);
+            setCurrentUser(user)
+        })
+        return unsubscribe;
+    })
 
     return (
         <UserContext.Provider value={value}>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
@@ -68,3 +68,5 @@ export const createAuthUserWithEmailAndPassword = async (email, password, displa
     }
 
 }
+
+export const subscribeToAuthChange = (callback)=> onAuthStateChanged(auth, callback);
