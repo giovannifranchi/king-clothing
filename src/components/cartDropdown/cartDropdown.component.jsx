b23feca6a21decc835d85a47875828c7712d7bca
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-02 18:18:46 +0200

    Add: cart rwactive working

--- src/components/cartDropdown/cartDropdown.component.jsx
+++ src/components/cartDropdown/cartDropdown.component.jsx
@@ -1,21 +1,35 @@
 import './cartDropdown.style.scss';
 
+import DropdownCard from '../dropdownCard/dropDownCard.component';
 import Button from '../button/button.component';
-import { UserContext } from '../../contexts/users.context';
 import { CartContext } from '../../contexts/cart.context';
 import { useContext } from 'react';
+
 
 
 const CartDropdown = () => {
 
     const {isOpen, cartItems, totalAmount} = useContext(CartContext);
 
+    const cartItemsArray = ()=>{
+        if(!Object.keys(cartItems).length) return [];
+        const list = [];
+        for(let item in cartItems) list.push(cartItems[item]);
+        return list;
+    }
+
+    const cart = cartItemsArray();
+
     return (
         <div className={`${isOpen ? 'cart-dropdown-container' : 'd-none'}`}>
 
             {
                 totalAmount ? 
-                (<div className='cart-items'></div>)
+                (<div className='cart-items'>
+                    {
+                        cart.map((item)=>{return <DropdownCard item={item}/>})
+                    }
+                </div>)
                 :
                 (<div className='empty-message'>No items in cart</div>)
             }
