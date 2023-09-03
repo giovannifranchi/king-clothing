47d1c8b676c17feea7a961e50aaa752dec26b461
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-03 12:11:29 +0200

    Add: checkout route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NavBar from './routes/navbar/navbar.component';
 import Shop from './routes/shop/shop.component';
 import Contacts from './routes/contacts/contacts.component';
 import Authentication from './routes/authentication/authentication.component';
+import Checkout from './routes/checkout/checkout.component';
 
 const App = ()=> {
 
@@ -16,6 +17,7 @@ const App = ()=> {
         <Route path='shop' element={<Shop/>}/>
         <Route path='signin' element={<Authentication/>}/>
         <Route path='contacts' element={<Contacts/>}/>
+        <Route path='checkout' element={<Checkout/>} />
       </Route>
     </Routes>
   )
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,9 @@
+import './checkout.style.scss'
+
+const Checkout = ()=>{
+    return (
+        <h1>this is the checkout page</h1>
+    )
+} 
+
+export default Checkout;
