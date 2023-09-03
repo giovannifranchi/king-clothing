14124fa3b786bb7812520b1b6422a18f83c162fd
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-03 19:24:36 +0200

    Refactor on shop route

--- src/components/categoriesSection/categoriesSection.component.jsx
+++ src/components/categoriesSection/categoriesSection.component.jsx
@@ -1,22 +1,35 @@
 import './categoriesSection.style.scss';
 import ProductCard from '../productCard/productCard.component';
+import { CategoriesContext } from '../../contexts/categories.context';
+import { useContext } from 'react';
 
 
 
-const CategoriesSection = ({ category, items }) => {
 
-    const getFirstFour = items.slice(0, 4);
+const CategoriesSection = () => {
+
+    const { categories } = useContext(CategoriesContext);
 
     return (
-        <div className='mt-3 mb-5'>
-            <h2>{category.toUpperCase()}</h2>
-            <div className='row gy-5'>
-                {getFirstFour.map((item) => (
-                    <div key={item.id} className='col-sm-6 col-md-3'>
-                        <ProductCard product={item}/>
-                    </div>
-                ))}
-            </div>
+        <div className='container'>
+            {
+                Object.keys(categories).map((category)=>{
+                    return (
+                        <div className='row mt-3 mb-5' key={category}>
+                            <h2>
+                                <span>{category}</span>
+                            </h2>
+                            {
+                                categories[category].slice(0, 4).map((item)=>(
+                                    <div key={item.id} className='col-sm-6 col-md-3'>
+                                        <ProductCard product={item}/>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,26 +1,12 @@
 import './shop.style.scss';
-import { useContext } from 'react';
-import { CategoriesContext } from '../../contexts/categories.context';
 import CategoriesSection from '../../components/categoriesSection/categoriesSection.component';
 
 
 const Shop = () => {
-
-    const { categories } = useContext(CategoriesContext);
-
     
-
     return (
         <div className='container'>
-            <div>
-                {Object.keys(categories).length ? (
-                    Object.keys(categories).map((category) => (
-                        <CategoriesSection items={categories[category]} category={category} key={category} />
-                    ))
-                ) : (
-                    <h3>...Loading</h3>
-                )}
-            </div>
+            <CategoriesSection />
         </div>
     )
 }
