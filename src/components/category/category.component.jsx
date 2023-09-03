a0f95f16f9086f02f009a96dbb3a58d7ac1f4fe4
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-03 20:10:41 +0200

    Add: routing completed

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import './category.style.scss';
 
 const Category = ({category})=> {
@@ -6,12 +7,14 @@ const Category = ({category})=> {
     const additionalClasses = id < 4 ? "col-md-4" : "col-md-6";
     return (
         <div className={`${basicClasses} ${additionalClasses} background-container`}>
+            <NavLink className='text-reset text-decoration-none' to={`shop/${title}`}>
             <div className='p-3 h-100 background d-flex align-items-center justify-content-center flex-column' style={{backgroundImage: `url(${imageUrl})`}}>
                 <div className='category-body p-3'>
                     <h3 className='text-center'>{title}</h3>
                     <h4 className='text-center'>SHOP NOW</h4>
                 </div>
             </div>
+            </NavLink>
         </div>
     )
 }
