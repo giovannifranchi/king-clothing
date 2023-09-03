657613fb89ff90f336afd28766c123e4bff00780
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-03 19:07:13 +0200

    Add: preview of four

--- src/components/categoriesSection/categoriesSection.component.jsx
+++ src/components/categoriesSection/categoriesSection.component.jsx
@@ -2,13 +2,16 @@ import './categoriesSection.style.scss';
 import ProductCard from '../productCard/productCard.component';
 
 
+
 const CategoriesSection = ({ category, items }) => {
+
+    const getFirstFour = items.slice(0, 4);
 
     return (
         <div className='mt-3 mb-5'>
             <h2>{category.toUpperCase()}</h2>
             <div className='row gy-5'>
-                {items.map((item) => (
+                {getFirstFour.map((item) => (
                     <div key={item.id} className='col-sm-6 col-md-3'>
                         <ProductCard product={item}/>
                     </div>
