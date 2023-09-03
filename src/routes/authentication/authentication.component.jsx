e86b74da652c86744f723a21c97123671622802e
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-03 21:33:34 +0200

    Fix: button font

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -5,8 +5,8 @@ import SignInForm from '../../components/signInForm/signInForm.component';
 const Authentication = () => {
 
     return (
-        <div className='container pt-5'>
-            <div className='row justify-content-center gap-5'>
+        <div className='container py-5 mt-5'>
+            <div className='row justify-content-center gap-5 py-5'>
                 <div className='col-sm-12 col-md-5'>
                     <SignInForm />
                 </div>
