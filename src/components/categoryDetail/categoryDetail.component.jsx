e757222143077acc37ac3eecaa479d0e34ea1f0c
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-05 16:19:41 +0200

    Add: general fixes to categories mapping

--- src/components/categoryDetail/categoryDetail.component.jsx
+++ src/components/categoryDetail/categoryDetail.component.jsx
@@ -9,13 +9,14 @@ const CategoryDetail = ()=> {
 
     const categories = useSelector(getCategories);
     const { category } = useParams();
+    const categoriesToMap = categories[category] || [];
 
     return(
         <div className='container mt-5 py-5'>
             <h2 className='mt-5 text-center'>{category.toUpperCase()}</h2>
             <div className='row gy-5 py-5'>
             {
-                    categories[category].map((item)=>(
+                    categoriesToMap.map((item)=>(
                         <div className='col-sm-6 col-md-3' key={item.id}>
                             <ProductCard product={item}/>
                         </div>
