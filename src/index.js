ef316e61c946d1ce40d15e48d43c6ac81609b9e1
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-05 12:20:25 +0200

    Add: store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/users.context';
 import { CategoriesProvider } from './contexts/categories.context';
@@ -13,7 +14,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <UserProvider>
           <CategoriesProvider>
