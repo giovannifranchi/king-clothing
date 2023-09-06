4bda705ab26d5f15d5b2f0b1e0530496bc425047
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-06 02:09:08 +0200

    move helper to reducer utils

--- src/store/cart/cart.reducer.js
+++ src/store/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { CART_ACTION_TYPES } from "./cart.types";
+import { addItemHelper, removeItemHelper, clearHelper } from "../../utils/reducer/reducer.utils";
 
 const INITIAL_STATE = {
     isOpen: false,
@@ -10,55 +11,15 @@ const INITIAL_STATE = {
 export const cartReducer = (state = INITIAL_STATE, action) => {
     const { type, payload } = action;
 
-    const addItemHelper = (item) => {
-        if (!(item.id in state.cartItems)){
-            return {
-                ...state.cartItems, [item.id]: {
-                    info: item,
-                    itemAmount: 1
-                }
-            }
-        }else {
-            return {
-                ...state.cartItems, [item.id]:{
-                    info: item,
-                    itemAmount: state.cartItems[item.id].itemAmount + 1
-                }
-            }
-        }
-    }
-
-    const removeItemHelper = (item)=> {
-        if(state.cartItems[item.id].itemAmount === 1){
-            const newItems = {...state.cartItems};
-            delete newItems[item.id];
-            return newItems;
-        }else {
-            return {
-                ...state.cartItems,
-                [item.id]:{
-                    info: item,
-                    itemAmount: state.cartItems[item.id].itemAmount - 1
-                }
-            }
-        }
-    }
-
-    const clearHelper = (item)=> {
-        const newItems = { ...state.cartItems };
-        delete newItems[item.id];
-        return newItems;
-    }
-
     switch (type) {
         case CART_ACTION_TYPES.TOGGLE_IS_OPEN:
             return { ...state, isOpen: payload };
         case CART_ACTION_TYPES.ADD_ITEM_TO_CART:
-            return { ...state, cartItems: addItemHelper(payload), totalAmount: state.totalAmount + 1 };
+            return { ...state, cartItems: addItemHelper(payload, state), totalAmount: state.totalAmount + 1 };
         case CART_ACTION_TYPES.REMOVE_ITEM_FROM_CART:
-            return { ...state, cartItems: removeItemHelper(payload), totalAmount: state.totalAmount - 1 };
+            return { ...state, cartItems: removeItemHelper(payload, state), totalAmount: state.totalAmount - 1 };
         case CART_ACTION_TYPES.CLEAR_ITEMS:
-            return { ...state, cartItems: clearHelper(payload), totalAmount: state.totalAmount - state.cartItems[payload.id].itemAmount };
+            return { ...state, cartItems: clearHelper(payload, state), totalAmount: state.totalAmount - state.cartItems[payload.id].itemAmount };
         default:
             return state;
     }
--- src/utils/reducer/reducer.utils.js
+++ src/utils/reducer/reducer.utils.js
@@ -1,1 +1,41 @@
-export const createAction = (type, payload)=>({type, payload});
+export const createAction = (type, payload) => ({ type, payload });
+
+export const addItemHelper = (item, state) => {
+    if (!(item.id in state.cartItems)) {
+        return {
+            ...state.cartItems, [item.id]: {
+                info: item,
+                itemAmount: 1
+            }
+        }
+    } else {
+        return {
+            ...state.cartItems, [item.id]: {
+                info: item,
+                itemAmount: state.cartItems[item.id].itemAmount + 1
+            }
+        }
+    }
+}
+
+export const removeItemHelper = (item, state) => {
+    if (state.cartItems[item.id].itemAmount === 1) {
+        const newItems = { ...state.cartItems };
+        delete newItems[item.id];
+        return newItems;
+    } else {
+        return {
+            ...state.cartItems,
+            [item.id]: {
+                info: item,
+                itemAmount: state.cartItems[item.id].itemAmount - 1
+            }
+        }
+    }
+}
+
+export const clearHelper = (item, state) => {
+    const newItems = { ...state.cartItems };
+    delete newItems[item.id];
+    return newItems;
+}
