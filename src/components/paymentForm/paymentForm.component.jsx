0473e78cdd27f193b009f1219a42f0d48137aa52
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-07 12:46:45 +0200

    Add: payment try working

--- src/components/paymentForm/paymentForm.component.jsx
+++ src/components/paymentForm/paymentForm.component.jsx
@@ -1,23 +1,33 @@
 import './paymentForm.style.scss';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import  Button  from '../button/button.component';
+import Button from '../button/button.component';
 
-const PaymentForm = ()=>{
+const PaymentForm = () => {
 
     const stripe = useStripe();
     const elements = useElements();
-    
-    const paymentHandler = async(e)=>{
+
+    const paymentHandler = async (e) => {
         e.preventDefault();
-        if(!stripe || !elements) return;
-        
+        if (!stripe || !elements) return;
+
+        const response = await fetch('/.netlify/functions/create-payment-intent', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ amount: 1000 })
+        }).then(res => res.json());
+
+        console.log(response);
+
     }
-    
+
     return (
-        <div>
-            <CardElement/>
-            <Button buttonType='inverted' text='buy now'/>
-        </div>
+        <form onSubmit={paymentHandler}>
+            <CardElement />
+            <Button buttonType='inverted' text='buy now' />
+        </form>
     )
 }
 
