042ed142cd8eb0a3e77bcc4df10e2e0d86d3fbb3
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-07 22:36:57 +0200

    Add: create action in ts

--- src/utils/reducer/reducer.utils.ts
+++ src/utils/reducer/reducer.utils.ts
@@ -1,2 +1,22 @@
-export const createAction = (type, payload) => ({ type, payload });
+
+export type ActionWithPayload<T, P> = {
+    type:T;
+    payload:P
+}
+
+export type Action<T> = {
+    type:T
+}
+
+//This is function overload in order to handle cases in which we don't have payload
+
+export function createAction<T extends string, P>(type: T, payload: P):ActionWithPayload<T, P>;
+
+export function createAction<T extends string>(type: T, payload: void):Action<T>;
+
+
+export function createAction<T extends string, P>(type: T, payload: P) {
+    return { type, payload }
+}
+
 
