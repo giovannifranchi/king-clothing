82d2c96ddc45698f4b6b8effe061a474b21382eb
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-07 22:57:51 +0200

    Add: migrate to categories action ts

---
+++ src/store/categories/categories.action.ts
@@ -0,0 +1,39 @@
+import { CATEGORIES_ACTION_TYPES } from "./categories.type"
+import { createAction, Action, ActionWithPayload } from "../../utils/reducer/reducer.utils"
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils"
+import { Category } from "./categories.type";
+import { Dispatch } from "react";
+import { DocumentData } from "firebase/firestore";
+
+
+export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>
+
+export type FetchCategoriesSuccess = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, CategoryData[]>
+
+export type FetchCategoriesFailure = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, Error>
+
+export type CategoryAction = FetchCategoriesStart | FetchCategoriesSuccess | FetchCategoriesFailure
+
+export type CategoryData = Category | DocumentData
+
+export const fetchCategoriesStart = ():FetchCategoriesStart => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoryArray: CategoryData[]):FetchCategoriesSuccess=>createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoryArray);
+
+export const fetchCategoriesFailure = (error:Error):FetchCategoriesFailure=>createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+
+export const fetchCategoriesAsync = ()=>{
+    return async (dispatch:Dispatch<CategoryAction>)=> {
+        dispatch(fetchCategoriesStart());
+        try {
+            
+            const categoriesArray = await getCategoriesAndDocuments();
+            dispatch(fetchCategoriesSuccess(categoriesArray));
+            
+        } catch (error) {
+            dispatch(fetchCategoriesFailure(error));
+        }
+    }
+}
+
--- src/store/categories/categories.type.ts
+++ src/store/categories/categories.type.ts
@@ -4,3 +4,15 @@ export enum CATEGORIES_ACTION_TYPES {
     FETCH_CATEGORIES_SUCCESS = 'FETCH_CATEGORIES_SUCCESS',
     FETCH_CATEGORIES_FAILED = 'FETCH_CATEGORIES_FAILED'
 }
+
+export type CategoryItem = {
+    id:number;
+    name:string;
+    imageUrl:string;
+    price:number;
+}
+
+export type Category = {
+    title:string,
+    items: CategoryItem[];
+}
