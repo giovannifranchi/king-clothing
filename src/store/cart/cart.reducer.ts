fecdf9d7f73cb21122065c13cf8cd5e64357a1a7
giovannifranchi <giovanni.franchi95@gmail.com>
2023-09-08 01:30:28 +0200

    Add: cart reducer typed

--- src/store/cart/cart.reducer.ts
+++ src/store/cart/cart.reducer.ts
@@ -1,4 +1,6 @@
-import { CART_ACTION_TYPES, ItemsMap } from "./cart.types";
+import { AnyAction } from "redux";
+import {  ItemsMap } from "./cart.types";
+import { addItemToCart, removeItemFromCart, clearItems, updateTotal, toggleIsOpen } from "./cart.action";
 
 export type CartState = {
     readonly isOpen: boolean;
@@ -13,21 +15,33 @@ const INITIAL_STATE = {
 };
 
 
-export const cartReducer = (state = INITIAL_STATE, action) => {
-    const { type, payload } = action;
+export const cartReducer = (state = INITIAL_STATE, action = {} as AnyAction):CartState => {
+    
+    if(toggleIsOpen.match(action)) return { ...state, isOpen: action.payload };
 
-    switch (type) {
-        case CART_ACTION_TYPES.TOGGLE_IS_OPEN:
-            return { ...state, isOpen: payload };
-        case CART_ACTION_TYPES.ADD_ITEM_TO_CART:
-            return { ...state, cartItems: payload, totalAmount: state.totalAmount + 1 };
-        case CART_ACTION_TYPES.REMOVE_ITEM_FROM_CART:
-            return { ...state, cartItems: payload, totalAmount: state.totalAmount - 1 };
-        case CART_ACTION_TYPES.CLEAR_ITEMS:
-            return { ...state, cartItems: payload};
-        case CART_ACTION_TYPES.UPDATE_TOTAL_AMOUNT:
-            return {...state, totalAmount:payload};
-        default:
-            return state;
-    }
+    if(addItemToCart.match(action)) return { ...state, cartItems: action.payload, totalAmount: state.totalAmount + 1 };
+
+    if(removeItemFromCart.match(action)) return { ...state, cartItems: action.payload, totalAmount: state.totalAmount - 1 };
+
+    if(clearItems.match(action)) return { ...state, cartItems: action.payload };
+
+    if(updateTotal.match(action)) return { ...state, totalAmount: action.payload };
+
+    return state;
 }
+
+
+// switch (type) {
+//     case CART_ACTION_TYPES.TOGGLE_IS_OPEN:
+//         return { ...state, isOpen: payload };
+//     case CART_ACTION_TYPES.ADD_ITEM_TO_CART:
+//         return { ...state, cartItems: payload, totalAmount: state.totalAmount + 1 };
+//     case CART_ACTION_TYPES.REMOVE_ITEM_FROM_CART:
+//         return { ...state, cartItems: payload, totalAmount: state.totalAmount - 1 };
+//     case CART_ACTION_TYPES.CLEAR_ITEMS:
+//         return { ...state, cartItems: payload};
+//     case CART_ACTION_TYPES.UPDATE_TOTAL_AMOUNT:
+//         return {...state, totalAmount:payload};
+//     default:
+//         return state;
+// }
